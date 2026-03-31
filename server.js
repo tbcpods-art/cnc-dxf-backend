1cbd56b3f2652c38d515c21edc824ba40c7c81d0
tbcpods-art <tbcpods@gmail.com>
2026-03-31 21:04:57 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Stripe = require("stripe");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.use(cors({
+  origin: "http://localhost:5500",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+app.options("/", cors());
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const cart = req.body.cart;
+
+    const line_items = cart.map(item => ({
+      price_data: {
+        currency: "gbp",
+        product_data: {
+          name: item.name
+        },
+        unit_amount: Math.round(item.price * 100)
+      },
+      quantity: item.qty
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items,
+      success_url: "http://localhost:5500/success.html",
+      cancel_url: "http://localhost:5500/cart.html"
+    });
+
+    res.json({ url: session.url });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating checkout session");
+  }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
